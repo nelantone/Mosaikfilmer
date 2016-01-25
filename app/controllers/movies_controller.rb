d5963d7ddb7214a4c043128e67c9af028b08904c
Toño Serna <antonelo@gmail.com>
2016-01-25 23:45:04 +0100

    add :image, typo

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -74,6 +74,6 @@ class MoviesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movie_params
-      params.require(:movie).permit(:name, :category, :country, :year, :rating, :opinion, :actors, :image)
+      params.require(:movie).permit(:name, :category, :country, :year, :rating, :opinion, :actors, :image, :remote_image_url)
     end
 end
