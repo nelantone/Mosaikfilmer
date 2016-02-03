ab7d154ea7f1962d0cb3b31a962bbcdc66cb6e2c
Toño Serna <antonelo@gmail.com>
2016-02-03 17:29:31 +0100

    Add validators and some little changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,12 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
   mount_uploader :image, ImageUploader
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :country, presence: true
+  validates :year, format: { with: /\A(19|20)\d{2}\z/,
+    message: "please write a correct year"}
+  validates :rating, presence: true, format: { with: /\A-?[0-5]+\.[0-9]+\z/,
+    message: "please write a correct rating (from 0.0 to 5.0)"}
 end
