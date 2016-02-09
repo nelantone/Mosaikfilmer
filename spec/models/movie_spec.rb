c4a1808dccc007d70996271447829540500fbf31
Toño Serna <antonelo@gmail.com>
2016-02-09 16:18:04 +0100

    do movie_spec & movie_view_edit_spec

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,9 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  #Woth's it or delete?
-  # it "has a valid factory" do
-  #   expect(assigns(:movies)).to be_valid
-  # end
+  it 'has a valid factory' do
+    @movie = FactoryGirl.create(:movie)
+    expect(@movie).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    name_nil = FactoryGirl.build(:movie, name: nil)
+    expect(name_nil).to_not be_valid
+  end
+
+  it 'is invalid without a category' do
+    category_nil =  FactoryGirl.build(:movie, category: nil)
+    expect(category_nil).to_not be_valid
+  end
+
+  it 'is invalid without a country' do
+    country_nil = FactoryGirl.build(:movie, country: nil)
+    expect(country_nil).to_not be_valid
+  end
+
+  it 'is a valid year between 1900 and 2099' do
+    valid_year = FactoryGirl.build(:movie, year: 1983)
+    expect(valid_year.year.to_s).to match(/\A(19|20)\d{2}\z/)
+  end
+
+  it 'is an invalid year' do
+    invalid_year= FactoryGirl.build(:movie, year: 160)
+    expect(invalid_year.year.to_s).to_not match(/\A(19|20)\d{2}\z/)
+  end
+
+  it 'is a valid score' do
+    valid_score = FactoryGirl.build(:movie, score: 4.5)
+    expect(valid_score.score.to_s).to match(/\A-?[0-5]+\.[0-9]+\z/)
+  end
+
+  it 'is a not valid score' do
+    invalid_score = FactoryGirl.build(:movie, score: 10)
+    expect(invalid_score.to_s).to_not match(/\A-?[0-5]+\.[0-9]+\z/)
+  end
 end
--- spec/views/movies/edit.html.erb_spec.rb
+++ spec/views/movies/edit.html.erb_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe "movies/edit", type: :view do
     @movie = assign(:movie, FactoryGirl.create(:movie))
   end
 
-  xit "renders the edit movie form" do
+  it "renders the edit movie form" do
     render
 
     assert_select "form[action=?][method=?]", movie_path(@movie), "post" do
 
       assert_select "input#movie_name[name=?]", "movie[name]"
 
-      #assert_select "input#movie_category[name=?]", "movie[category]"
+      assert_select "select#movie_category[name=?]", "movie[category]"
 
-      #assert_select "input#movie_country[name=?]", "movie[country]"
+      assert_select "select#movie_country[name=?]", "movie[country]"
 
       assert_select "input#movie_year[name=?]", "movie[year]"
 
-      #assert_select "input#movie_score[name=?]", "movie[score]"
+      assert_select "select#movie_score[name=?]", "movie[score]"
 
-      #assert_select "input#movie_opinion[name=?]", "movie[opinion]"
+      assert_select "textarea#movie_opinion[name=?]", "movie[opinion]"
 
       assert_select "input#movie_actors[name=?]", "movie[actors]"
     end
