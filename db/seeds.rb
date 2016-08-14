88a0c732254bafceb2f23f57a65d21e753f99173
Toño Serna <antonelo@gmail.com>
2016-08-14 02:46:33 +0200

    4.6 Make front-end changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Movie.create(
+  name: "Test",
+  category: "action",
+  country: "Germany",
+  year: 2016,
+  score: 4.5,
+  opinion: "good",
+  actors: "Luigi Bros",
+  user_id: "10",
+  image: "poster1.jpg",
+  )
+
+Movie.create(
+  name: "Test2",
+  category: "action",
+  country: "Spain",
+  year: 2016,
+  score: 5.0,
+  opinion: "good",
+  actors: "Curro Gimenez",
+  user_id: "10",
+  image: "poster2.jpg")
+
+Movie.create(
+  name: "Test3",
+  category: "action",
+  country: "Italy",
+  year: 2016,
+  score: 3.5,
+  opinion: "good",
+  actors: "Luigi Malito",
+  user_id: "10",
+  image: "poster3.jpg")
+
+Movie.create(
+  name: "Test4",
+  category: "action",
+  country: "Portugal",
+  year: 2016,
+  score: 4.0,
+  opinion: "good",
+  actors: "Luigi Bros",
+  user_id: "10",
+  image: "poster5.jpg")
+
+Movie.create(
+  name: "Test5",
+  category: "action",
+  country: "USA",
+  year: 2016,
+  score: 4.5,
+  opinion: "good",
+  actors: "Luigi Bros",
+  user_id: "10",
+  image:  "movie2.jpg")
